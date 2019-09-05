9a6fd9db1679f644a337090508080e4eea624975
曹弘扬 <296786475@qq.com>
2019-09-05 16:39:41 +0800

    iOS 版本显示地图。

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,5 +1,6 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import <AMapFoundationKit/AMapFoundationKit.h>
 
 @implementation AppDelegate
 
@@ -7,6 +8,7 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+   [AMapServices sharedServices].apiKey = @"b815c1702a908e49d4cc0b4ef9c73111";
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
